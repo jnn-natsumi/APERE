e644149e347073cc62335a3b45499d0aba63d393
natsumi minowa <apple@appurunoMacBook-Air.local>
2020-07-26 23:48:22 +0900

    Add gmap and Add myprofile

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
   # get 'users/:id/favorite' => 'users#favorite'
 
   root to: 'homes#top'
+  get '/map_request', to: 'maps#map', as: 'map_request'
+  resources :maps, only: [:index]
   get 'about' => 'homes#about'
+  get 'myprofile' => 'homes#myprofile'
 
   resources :posts do
     resource  :like, only:[:create, :destroy]
