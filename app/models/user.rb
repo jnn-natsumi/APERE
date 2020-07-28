911d3aaae006735a907619f00954ef9aa70b27ae
natsumi minowa <apple@appurunoMacBook-Air.local>
2020-07-28 23:15:53 +0900

    Update gmap

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  def self.guest
+    find_or_create_by!(name: 'ゲストユーザー', email: 'guest@example.com', birthplace: '東京都') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+  end
+
   attachment :profile_image, destroy: false
   validates :name, presence: true, length: {in: 2..20 }
   validates :profile, length: { maximum: 140 }
