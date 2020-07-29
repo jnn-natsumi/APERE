0446b223f2bc101a91a2e67d7d703d7a93d9cf00
natsumi minowa <apple@appurunoMacBook-Air.local>
2020-07-29 22:41:24 +0900

    Update indent

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,64 +1,63 @@
 class PostsController < ApplicationController
     before_action :authenticate_user!,only: [:create,:edit,:update,:destroy,:index]
 
-    # ２になおす　7/29
 
-	def index
-        @user = current_user
-        @posts = Post.page(params[:page]).per(10).order(created_at: :desc)
-        regions = []
-        @posts.each do |post|
-            regions << post.tag_list
-        end
-        @regions = regions.uniq
-        # binding.pry
-        if params[:tag_name]
-            @posts = Post.tagged_with("#{params[:tag_name].first}")
-        end
-    end
+  def index
+      @user = current_user
+      @posts = Post.page(params[:page]).per(10).order(created_at: :desc)
+      regions = []
+      @posts.each do |post|
+          regions << post.tag_list
+      end
+      @regions = regions.uniq
+      # binding.pry
+      if params[:tag_name]
+          @posts = Post.tagged_with("#{params[:tag_name].first}")
+      end
+  end
 
-    def new
-        @post = Post.new
-    end
+  def new
+      @post = Post.new
+  end
 
-    def create
-        @post = Post.new(post_params)
-        @post.user_id = current_user.id
-        if  @post.save
-            redirect_to user_path(current_user), notice: "キロクを投稿しました！"
-        else
-            render action: :new
-        end
-    end
+  def create
+      @post = Post.new(post_params)
+      @post.user_id = current_user.id
+      if  @post.save
+          redirect_to user_path(current_user), notice: "キロクを投稿しました！"
+      else
+          render action: :new
+      end
+  end
 
-    def edit
-        @post = Post.find(params[:id])
-    end
+  def edit
+      @post = Post.find(params[:id])
+  end
 
-    def update
-        @post = Post.find(params[:id])
-        if @post.update(post_params)
-            redirect_to post_path(@post.id), notice: "キロクを編集しました！"
-        else
-            render action: :edit
-        end
-    end
+  def update
+      @post = Post.find(params[:id])
+      if @post.update(post_params)
+          redirect_to post_path(@post.id), notice: "キロクを編集しました！"
+      else
+          render action: :edit
+      end
+  end
 
-    def show
-        @post = Post.find(params[:id])
-        @user = User.find_by(id: @post.user_id)
-        @comment = Comment.new
-        @comments = @post.comments
-    end
+  def show
+      @post = Post.find(params[:id])
+      @user = User.find_by(id: @post.user_id)
+      @comment = Comment.new
+      @comments = @post.comments
+  end
 
-    def destroy
-        @post = Post.find(params[:id])
-        @post.destroy
-        redirect_to user_path(current_user)
-    end
+  def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+      redirect_to user_path(current_user)
+  end
 
-    private
-    def post_params
-        params.require(:post).permit(:travel_spot, :cost, :tag, :point_a, :point_b, :explanation, :travel_image, :tag_list)
-    end
+  private
+  def post_params
+      params.require(:post).permit(:travel_spot, :cost, :tag, :point_a, :point_b, :explanation, :travel_image, :tag_list)
+  end
 end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,16 +2,16 @@ class RelationshipsController < ApplicationController
 
 	# follow フォローしている follower フォローされてる
 	def create
-	  	follow = current_user.active_relationships.build(follower_id: params[:user_id])
-	  	follow.save
-	  	redirect_to request.referrer
-  	# request.referrerで１つ前のURLを返す。
-    end
+  	follow = current_user.active_relationships.build(follower_id: params[:user_id])
+  	follow.save
+  	redirect_to request.referrer
+		# request.referrerで１つ前のURLを返す。
+  end
 
-    def destroy
-	  	follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
-	  	follow.destroy
-	  	redirect_to request.referrer
-    end
+  def destroy
+  	follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
+  	follow.destroy
+  	redirect_to request.referrer
+  end
 
 end
