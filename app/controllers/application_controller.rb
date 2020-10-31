ccd7fb41db6e19ae4aa86434a398fc7bcb6a6b66
natsumi minowa <apple@appurunoMacBook-Air.local>
2020-10-31 23:28:07 +0900

    Fix postpage new edit show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,6 @@
 class ApplicationController < ActionController::Base
 before_action :configure_permitted_parameters, if: :devise_controller?
 
-
-  def after_sign_in_path_for(resource)
-    user_path(current_user)
-  end
-
-  def after_sign_out_path_for(resource_or_scope)
-    flash[:notice] = "Signed out successfully."
-    root_path
-  end
-
-
 protected
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :birthplace])
